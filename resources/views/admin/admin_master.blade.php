87c190f43d452eed9f6025f815f08e83eb91084d
exiaquan <42194364+exiaquan@users.noreply.github.com>
2023-09-16 15:50:22 +0700

    asset

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -1491,27 +1491,27 @@
 	</div>
 	<!--end switcher-->
 	<!-- Bootstrap JS -->
-	<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<script src="{{asset('backend/assets/js/bootstrap.bundle.min.js')}}"></script>
 	<!--plugins-->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-	<script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-	<script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-	<script src="assets/plugins/chartjs/js/Chart.min.js"></script>
-	<script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-	<script src="assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-	<script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
-	<script src="assets/plugins/jquery-knob/excanvas.js"></script>
-	<script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
+	<script src="{{asset('backend/assets/js/jquery.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/chartjs/js/Chart.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+    <script src="{{asset('backend/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/sparkline-charts/jquery.sparkline.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/jquery-knob/excanvas.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/jquery-knob/jquery.knob.js')}}"></script>
 	  <script>
 		  $(function() {
 			  $(".knob").knob();
 		  });
 	  </script>
-	  <script src="assets/js/index.js"></script>
+	  <script src="{{asset('backend/assets/js/index.js')}}"></script>
 	<!--app JS-->
-	<script src="assets/js/app.js"></script>
+	<script src="{{asset('backend/assets/js/app.js')}}"></script>
 </body>
 
 </html>
